88c368a5be05f87e2d1698daa62fdca392ef7f8c
nailul.firdaus <Nailul.Firdaus@bni.co.id>
2023-03-15 15:54:14 +0700

    Add dashboard page

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,15 @@ export default function Home() {
         isSidebarOpen={isSidebarOpen}
         onClickToggleNavbar={sidebarHandler}
       />
-      <Sidebar isSidebarOpen={isSidebarOpen} activeMenu="dashboard" />
+      <div className="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
+        <Sidebar isSidebarOpen={isSidebarOpen} activeMenu="dashboard" />
+        <div
+          id="main-content"
+          className="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900"
+        >
+          <div className="p-3">CONTENT DASHBOARD</div>
+        </div>
+      </div>
     </>
   );
 }
